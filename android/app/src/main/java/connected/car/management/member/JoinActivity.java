7ad17a605fc0b6da3f84b8c0f967550de7161173
leeyj341 <leeyj3794@gmail.com>
2020-05-16 21:04:34 +0900

    * 회원가입 추가

--- android/app/src/main/java/connected/car/management/member/JoinActivity.java
+++ android/app/src/main/java/connected/car/management/member/JoinActivity.java
@@ -1,13 +1,27 @@
 package connected.car.management.member;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import connected.car.management.R;
 
 public class JoinActivity extends AppCompatActivity {
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+
+        spinner = findViewById(R.id.spinner_gender);
+        setSpinner();
+    }
+
+    public void setSpinner() {
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.gender, android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
--- android/app/src/main/java/connected/car/management/member/JoinAsync.java
+++ android/app/src/main/java/connected/car/management/member/JoinAsync.java
@@ -1,4 +1,19 @@
 package connected.car.management.member;
 
-public class JoinAsync {
+import android.os.AsyncTask;
+
+import okhttp3.OkHttpClient;
+
+public class JoinAsync extends AsyncTask<Void, Void, String> {
+
+    @Override
+    protected String doInBackground(Void... voids) {
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
 }
