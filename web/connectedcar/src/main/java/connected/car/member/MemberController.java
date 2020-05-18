bf9a05d47f7f952a126c8d4f5268990bd47d53ba
leeyj341 <leeyj3794@gmail.com>
2020-05-18 16:18:05 +0900

    * 사용자 회원 가입 완료

--- web/connectedcar/src/main/java/connected/car/member/MemberController.java
+++ web/connectedcar/src/main/java/connected/car/member/MemberController.java
@@ -5,8 +5,10 @@ import java.io.IOException;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -16,11 +18,13 @@ public class MemberController {
 	MemberService service;
 
 	@RequestMapping(value="/member/join.do", method=RequestMethod.POST, produces="application/json;charset=UTF-8")
-	public String joinMember(String vo) {
+	public @ResponseBody String joinMember(@RequestBody String vo) {
 		ObjectMapper mapper = new ObjectMapper();
 		int result = 0;
 		try {
+			System.out.println(vo);
 			MemberVO memberVO = mapper.readValue(vo, MemberVO.class);
+			System.out.println(memberVO.toString());
 			result = service.joinMember(memberVO);
 		} catch (IOException e) {
 			e.printStackTrace();
