8cd22e425a23b6b904e7ef37464680f849f2e32d
leeyj341 <leeyj3794@gmail.com>
2020-05-18 11:00:48 +0900

    *  안드로이드 MemberVO 추가

--- android/app/src/main/java/connected/car/management/member/MemberVO.java
+++ android/app/src/main/java/connected/car/management/member/MemberVO.java
@@ -1,4 +1,12 @@
 package connected.car.management.member;
 
 public class MemberVO {
+    String user_id;
+    String car_id;
+    String user_password;
+    String user_name;
+    String user_birthdate;
+    String user_gender;
+    String driver_license;
+
 }
