bbff7b53568e8abff7f8a633921290c78199e5f5
myomi <myomi0618@gmail.com>
2020-05-19 17:57:24 +0900

    * remote control package 생성 및 명칭 변경

--- android/app/src/main/java/connected/car/management/control/CarExpendable.java
+++ android/app/src/main/java/connected/car/management/control/CarExpendable.java
@@ -1,4 +1,4 @@
-package connected.car.management;
+package connected.car.management.control;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -7,13 +7,15 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import connected.car.management.R;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class status extends Fragment {
+public class CarExpendable extends Fragment {
 
-    public status() {
+    public CarExpendable() {
         // Required empty public constructor
     }
 
@@ -22,6 +24,6 @@ public class status extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_status, container, false);
+        return inflater.inflate(R.layout.fragment_car_expendablet, container, false);
     }
 }
--- android/app/src/main/java/connected/car/management/control/CarRemoteControl.java
+++ android/app/src/main/java/connected/car/management/control/CarRemoteControl.java
@@ -1,4 +1,4 @@
-package connected.car.management;
+package connected.car.management.control;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -7,13 +7,15 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import connected.car.management.R;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class control extends Fragment {
+public class CarRemoteControl extends Fragment {
 
-    public control() {
+    public CarRemoteControl() {
         // Required empty public constructor
     }
 
@@ -22,6 +24,6 @@ public class control extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_control, container, false);
+        return inflater.inflate(R.layout.fragment_car_remote_control, container, false);
     }
 }
