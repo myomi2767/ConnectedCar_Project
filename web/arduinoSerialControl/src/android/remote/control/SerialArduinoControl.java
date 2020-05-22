41b119e2447451bcd07f5db4dda55f15f9d0753c
leeyj341 <leeyj3794@gmail.com>
2020-05-22 09:38:21 +0900

    * 오류 수정

--- web/arduinoSerialControl/src/android/remote/control/SerialArduinoControl.java
+++ web/arduinoSerialControl/src/android/remote/control/SerialArduinoControl.java
@@ -18,7 +18,7 @@ public class SerialArduinoControl {
 	}
 	public void connect(String portName) {
 		try {
-			commPortIdentifier commPortIdentifier = CommPortIdentifier.getPortIdentifier(portName);
+			CommPortIdentifier commPortIdentifier = CommPortIdentifier.getPortIdentifier(portName);
 			if(commPortIdentifier.isCurrentlyOwned()) {
 				System.out.println("포트 사용 불가");
 			}else {
