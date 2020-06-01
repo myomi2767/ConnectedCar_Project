a64f94f0138035475c77796196a93e58dcdba266
snowcuphea <snowcuphea@gmail.com>
2020-06-01 12:38:07 +0900

    * pull받기 위한 커밋

--- android/app/src/main/java/connected/car/management/period/PeriodExchangeActivity.java
+++ android/app/src/main/java/connected/car/management/period/PeriodExchangeActivity.java
@@ -13,5 +13,8 @@ public class PeriodExchangeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_period_exchange);
 
         //리사이클러뷰로 부품리스트 가져와서 보여줘야 합니다.
+        //어댑터로 만들고, 각 항목에는 [선택] 버튼이 있으며 선택 시 맨 위에 setText되고
+        //[선택]버튼을 누를 때마다 setText되어 최종값만 반영됩니다.
+        //그리고 [확인] 버튼을 누르면 setText된 부품이 db에 update되고 액티비티가 꺼집니다.
     }
 }
